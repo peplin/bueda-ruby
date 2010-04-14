4b86935fceb7404f8a628a49a6f505d881e42d29
Christopher Peplin <peplin@bueda.com>
2010-04-14 10:15:22 -0400

    Test code for models.

--- lib/bueda/model.rb
+++ lib/bueda/model.rb
@@ -2,43 +2,53 @@ module Bueda
   class Client
     include HTTParty
     base_uri = 'http://api.bueda.com'
+    format :json
 
     def initialize(api_key)
       self.class.default_params :api_key => api_key
     end
+
+    def version()
+      self.class.get('/version')
+    end
   end
 
-  class Tags
+  class Tags < Client
     @@ATTRIBUTES = [:tags]
     attr_accessor *@@ATTRIBUTES
 
     def initialize(tags)
+      self.class.default_prams :tags => tags
     end
 
-    def enrich()
+    def enrich(options={})
+      self.class.get('/enriched', options)
     end
 
-    def get_categories()
+    def categories()
+      self.class.get('/categories', options)
     end
 
     def split()
+      self.class.get('/split', options)
     end
 
-    def get_concepts()
+    def concepts()
+      self.class.get('/semantics', options)
     end
   end
   
-  class EnrichedTag
+  class EnrichedTag < Client
     @@ATTRIBUTES = [:canonical, :categories, :concepts, :split]
     attr_accessor *@@ATTRIBUTES
   end
 
-  class Category
+  class Category < Client
     @@ATTRIBUTES = [:name, :confidence]
     attr_accessor *@@ATTRIBUTES
   end
 
-  class SemanticConcept
+  class SemanticConcept < Client
     @@ATTRIBUTES = [:id, :name, :original, :types]
     attr_accessor *@@ATTRIBUTES
   end
